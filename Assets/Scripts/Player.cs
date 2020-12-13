0e1aaaae5cba85d7439077cc29a998b2f263bbd5
Jianghui Dai <jianghuidai@outlook.com>
2020-12-13 15:35:51 -0800

    update player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    private Camera _viewCamera;
+    private PlayerController _controller;
+
+    private void Start()
+    {
+        _controller = GetComponent<PlayerController>();
+        _viewCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        var moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        var moveVelocity = moveInput.normalized * moveSpeed;
+        _controller.Move(moveVelocity);
+
+        var ray = _viewCamera.ScreenPointToRay(Input.mousePosition);
+        var groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            var point = ray.GetPoint(rayDistance);
+            // Debug.DrawLine(ray.origin, point, Color.magenta);
+            _controller.LookAt(point);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _myRigidbody;
+    private Vector3 _velocity;
+
+    private void Start()
+    {
+        _myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        _velocity = velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        var heightCorrectedPoint=  new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        _myRigidbody.MovePosition(_myRigidbody.position + _velocity * Time.fixedDeltaTime);
+    }
+}
